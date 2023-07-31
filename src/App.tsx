6854e08755612527f85d833b2b8d98fc67162876
Aaron Cox <aaron@greymass.com>
2023-07-31 14:25:44 -0700

    Updating to 1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,17 @@ import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import wharfLogo from './assets/wharf.svg'
 import './App.css'
-import SessionKit, { BrowserLocalStorage, Session } from '@wharfkit/session'
+import { Chains, Session, SessionKit } from '@wharfkit/session'
 import { WalletPluginAnchor } from '@wharfkit/wallet-plugin-anchor'
-import { WalletPluginCloudWallet } from '@wharfkit/wallet-plugin-cloudwallet'
 import WebRenderer from '@wharfkit/web-renderer'
 import React from 'react'
 
 const sessionKit = new SessionKit({
   appName: 'demo',
-  chains: [
-      {
-          id: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-          url: 'https://eos.greymass.com',
-      },
-      {
-          id: '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11',
-          url: 'https://telos.greymass.com',
-      },
-      {
-          id: '8fc6dce7942189f842170de953932b1f66693ad3788f766e777b6f9d22335c02',
-          url: 'https://api.uxnetwork.io',
-      },
-      {
-          id: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
-          url: 'https://wax.greymass.com',
-      },
-  ],
+  chains: [Chains.Jungle4],
   ui: new WebRenderer(),
   walletPlugins: [
       new WalletPluginAnchor(),
-      new WalletPluginCloudWallet(),
   ],
 })
 
@@ -63,7 +44,7 @@ function App() {
           data: {
               from: session.actor,
               to: 'teamgreymass',
-              quantity: '0.00000001 WAX',
+              quantity: '0.0001 EOS',
               memo: 'Yay WharfKit! Thank you <3',
           },
       }
